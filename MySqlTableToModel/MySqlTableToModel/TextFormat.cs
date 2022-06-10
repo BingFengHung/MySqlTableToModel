6d30b0e38df34be7bf827e968eff4dc80ae231ce
BingFengHung <joe94008@gmail.com>
2022-06-10 16:21:43 +0800

    <refactor>: SnakeCase 轉為 PascalCase 抽離出來為一個靜態類別方法

---
+++ MySqlTableToModel/MySqlTableToModel/TextFormat.cs
@@ -0,0 +1,15 @@
+﻿using System.Globalization;
+
+namespace MySqlTableToModel
+{
+    static class TextFormat
+    {
+        public static string SnakeCaseToPascalCase(string text)
+        {
+            string convert = text.Replace("_", " ");
+            convert = CultureInfo.CurrentCulture.TextInfo.ToTitleCase(convert);
+            convert = convert.Replace(" ", "");
+            return convert;
+        }
+    }
+}
